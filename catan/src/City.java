ee9f9443023b6e6415bf0946d84a277c8f3be90a
neryapy <99277845+neryapy@users.noreply.github.com>
2024-08-10 20:58:07 +0300

    Delete catan directory

--- catan/src/City.java
+++
@@ -1,8 +0,0 @@
-public class City {
-    private int location_res;
-    private int location_vertex;
-    public City(int location_res, int location_vertex){
-        this.location_res=location_res;
-        this.location_vertex=location_vertex;
-    }
-}
